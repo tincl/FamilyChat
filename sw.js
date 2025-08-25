1d0e21ef1a7fd4de6993b5d120127866764a219d
unknown <tincl00@gmail.com>
2025-08-25 20:23:53 +0900

    PWA

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-const CACHE = "mini-chat-v1";
-const ASSETS = ["/", "/index.html", "/manifest.json"];
-self.addEventListener("install", (e) => {
+const CACHE = "familychat-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
